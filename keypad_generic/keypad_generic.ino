ee6cb8c982d6de4f11e662bd417b50f27e7f85f2
Dardo Camaño <dardocam@gmail.com>
2024-08-11 18:44:33 -0300

    ok

---
+++ keypad_generic/keypad_generic.ino
@@ -0,0 +1,59 @@
+#include "Adafruit_Keypad.h"
+
+const byte ROWS = 4; // rows
+const byte COLS = 4; // columns
+//define the symbols on the buttons of the keypads
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {11, 10, 9, 8}; //connect to the column pinouts of the keypad
+
+//initialize an instance of class NewKeypad
+Adafruit_Keypad customKeypad = Adafruit_Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+void setup() {
+  Serial.begin(9600);
+  customKeypad.begin();
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  customKeypad.tick();
+
+  while(customKeypad.available()){
+    keypadEvent e = customKeypad.read();
+    Serial.print((char)e.bit.KEY);
+    if(e.bit.EVENT == KEY_JUST_PRESSED) Serial.println(" pressed");
+    else if(e.bit.EVENT == KEY_JUST_RELEASED) Serial.println(" released");
+  }
+
+  delay(10);
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
+  routine is run between each time loop() runs, so using delay inside loop can
+  delay response. Multiple bytes of data may be available.
+*/
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    if(inChar == 'c'){
+        digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(1000);                       // wait for a second
+        digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+        delay(1000); 
+        digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(1000);                       // wait for a second
+        digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+        delay(1000); 
+    }
+    Serial.println(inChar);
+  }
+}
--- public/javascripts/socket.io/myapp.js
+++ public/javascripts/socket.io/myapp.js
@@ -1,5 +1,43 @@
+var numbers = [];
+var key = "5582";
+var resp = "";
+var data = "";
 const socket = io();
 socket.on("arduino:data", function (dataSerial) {
-  console.log(dataSerial.value);
-  //   document.getElementById('serialData').innerHTML = dataSerial.value;
+  // console.log(dataSerial.value);
+  if (dataSerial.value.includes("release")) {
+    // console.log(dataSerial.value.charAt(0));
+    const n = dataSerial.value.charAt(0);
+    data = dataSerial.value.charAt(0);
+    numbers.push(n);
+  }
+  if (numbers.length <= 4) {
+    document.querySelector("p").innerHTML =
+      dataSerial.value.charAt(0) + "-" + numbers.length;
+  }
+
+  if (numbers.length == 4) {
+    numbers.forEach((nms) => {
+      console.log(nms);
+      resp = resp + nms;
+    });
+    if (resp == key) {
+      console.log("====================================");
+      console.log("WINNER");
+      console.log("====================================");
+      document.querySelector("p").innerHTML = "Complete";
+      setTimeout(() => {
+        socket.emit("complete", "finish");
+      }, 2000);
+    } else {
+      console.log("====================================");
+      console.log("LOOSER");
+      console.log("====================================");
+      document.querySelector("p").innerHTML = "Bad request";
+      setTimeout(() => {
+        location.reload();
+      }, 4000);
+    }
+  }
+  // }
 });
